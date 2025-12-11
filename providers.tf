2087fb694a864d4169198b75539c1f89dd912207
AlexPires1 <alexpirespereira747@gmail.com>
2025-12-10 22:32:22 -0300

    chore: fazendo a verificacao no s3

--- providers.tf
+++ providers.tf
@@ -5,12 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
-  # backend "s3" {
-  #   # Recomendado para CI/CD: Guardar o estado num bucket S3 (crie o bucket manualmente na AWS Academy antes)
-  #   # bucket = "nome-do-seu-bucket-terraform-state"
-  #   # key    = "rds/terraform.tfstate"
-  #   # region = "us-east-1"
-  # }
+  backend "s3" {
+    # Recomendado para CI/CD: Guardar o estado num bucket S3 (crie o bucket manualmente na AWS Academy antes)
+    bucket = "techchallenge-fase3-terraform-state-seunome"
+    key    = "rds/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
